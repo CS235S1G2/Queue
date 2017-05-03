90f9291a2ac70f0b2053425a21cf149658e48b34
jeremyc2 <goldenmeso@gmail.com>
2017-05-03 12:34:23 -0600

    Done

--- stock.cpp
+++ stock.cpp
@@ -1,15 +1,16 @@
 /***********************************************************************
- * Implementation:
- *    STOCK
- * Summary:
- *    This will contain the implementation for stocksBuySell() as well
- *    as any other function or class implementation you need
- * Author
- *    <your names here>
- **********************************************************************/
+* Implementation:
+*    STOCK
+* Summary:
+*    This will contain the implementation for stocksBuySell() as well
+*    as any other function or class implementation you need
+* Author
+*    <your names here>
+**********************************************************************/
 
 #include <iostream>    // for ISTREAM, OSTREAM, CIN, and COUT
 #include <string>      // for STRING
+#include <sstream>
 #include <cassert>     // for ASSERT
 #include "stock.h"     // for STOCK_TRANSACTION
 #include "dollars.h"   // used to handle currency
@@ -17,62 +18,112 @@
 #include <queue>
 using namespace std;
 
-/************************************************
- * STOCKS BUY SELL
- * The interactive function allowing the user to
- * buy and sell stocks
- ***********************************************/
-void stocksBuySell()
+queue <int> qShares;
+queue <Dollars> qDollars;
+queue <string> qTransactionHistory;
+
+void buyStock(int shares, Dollars dollars)
 {
-   // instructions
-   cout << "This program will allow you to buy and sell stocks. "
-        << "The actions are:\n";
-   cout << "  buy 200 $1.57   - Buy 200 shares at $1.57\n";
-   cout << "  sell 150 $2.15  - Sell 150 shares at $2.15\n";
-   cout << "  display         - Display your current stock portfolio\n";
-   cout << "  quit            - Display a final report and quit the program\n";
-
-   string instruction;
-   char token;
-   int shares;
-   string stringDollars;
-   cin >> instruction;
-   while (instruction != "quit")
-   {
-	   int i = 0;
-	   switch (token)
-	  {
-		   case 'b':
-			   i + 4;
-			   break;
-		   case 's':
-			   i + 5;
-		   break;
-	   }
-	   if (i == 4)
-	   {
-		   while (isalnum(instruction[i]))
-		   {
-			   numberOfShares.append(1, instruction[i]);
-			   i++;
-		   }
-		   while (i < instruction.length())
-		   {
-			   stringDollars.append(1, instruction[i]);
-			   i++;
-		   }
-	   }
-	   else if (i == 5)
-	   {
-
-	   }
-   }
+	qShares.push(shares);
+	qDollars.push(dollars);
 }
 
-// TODO delete main
-int main()
+Dollars sellStock(int sharesToBeSold, Dollars dollars)
 {
-	return 0;
-};
+	int soldShares = 0;
+	Dollars proceeds = 0;
+	// While we haven't sold enough shares
+	while (soldShares < sharesToBeSold)
+	{
+		// while adding one transaction still won't be enough keep 
+		// adding transactions
+		while ((soldShares + qShares.front()) < sharesToBeSold)
+		{
+			// add shares of one transaction
+			soldShares += qShares.front();
+			// figure out the proceeds 
+			proceeds += (dollars - qDollars.front()) * qShares.front();
+			// remove that transaction
+			qShares.pop();
+			qDollars.pop();
+			// update the count of the shares we still need to sell
+			sharesToBeSold -= soldShares;
+		}
+		// adding one is more than enough so add part of a transaction
+		// get rid of some of the shares in the front transaction of the queue
+		qShares.front() -= (sharesToBeSold);
+		soldShares += sharesToBeSold;
+		// proceeds = buying price * the rest of the shares we need to sell
+		proceeds += (dollars - qDollars.front()) * (sharesToBeSold);
+	}
+		// We sold all the shares we needed to sell, update the record
+		sharesToBeSold = 0;
+		stringstream ss;
+		string s1 = "Sold ";
+		string s2 = " shares at ";
+		string s3 = " for a profit of ";
+		string s4 = " for a loss of ";
+		// We made a profit
+		if (proceeds >= 0)
+			ss << s1 << soldShares << s2 << dollars << s3 << proceeds;
+		// We made a loss
+		else
+		{
+			proceeds = proceeds * -1;
+			ss << s1 << soldShares << s2 << dollars << s4 << proceeds;
+		}
+		s1 = ss.str();
+		qTransactionHistory.push(s1);
+}
 
+void display()
+{
+	// while the queue is not empty
+	while(!qTransactionHistory.empty())
+	{
+		cout << qTransactionHistory.front() << endl;
+		qTransactionHistory.pop();
+	}
+}
+
+/************************************************
+* STOCKS BUY SELL
+* The interactive function allowing the user to
+* buy and sell stocks
+***********************************************/
+void stocksBuySell()
+{
+	// instructions
+	cout << "This program will allow you to buy and sell stocks. "
+		<< "The actions are:\n";
+	cout << "  buy 200 $1.57   - Buy 200 shares at $1.57\n";
+	cout << "  sell 150 $2.15  - Sell 150 shares at $2.15\n";
+	cout << "  display         - Display your current stock portfolio\n";
+	cout << "  quit            - Display a final report and quit the program\n";
+
+	Dollars proceeds;
+	stringstream ss;
+	string instruction;
+	int shares = 0;
+	string command;
+	Dollars dollars;
+	while (instruction != "quit")
+	{
+		getline(cin, instruction);
+		istringstream ss(instruction);
+		ss >> command >> shares >> dollars;
+		if (command == "buy")
+		{
+			buyStock(shares, dollars);
+		}
+		else if (command == "sell")
+		{
+			proceeds = sellStock(shares, dollars);
+		}
+		else if (command == "display")
+		{
+			display();
+		}
+	}
+}
 
